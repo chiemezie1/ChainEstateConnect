424eadcab56d3fe9593e2fe6a30dcab03a849d08
chiemezie1 <chiemezieagbo1@gmail.com>
2023-12-20 17:32:41 +0000

    smart contract done

--- chainestateconnect/contracts/Contract.sol
+++ chainestateconnect/contracts/Contract.sol
@@ -2,7 +2,31 @@
 pragma solidity ^0.8.9;
 
 contract RealEstateProperty {
-    struct Property{
+    constructor() {
+        contractOwner = msg.sender;
+    }
+
+    address public contractOwner;
+    uint256 public propertyIndex;
+    uint256 public reviewsCount;
+    uint256 public commissionRate = 5; // 5% commission rate
+    mapping(address => uint256) public pendingWithdrawals;
+    mapping(uint256 => Property) private properties;
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == contractOwner,
+            "Only the contract owner can perform this action"
+        );
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid new owner address");
+        contractOwner = newOwner;
+    }
+
+    struct Property {
         uint256 productId;
         string productTitle;
         address owner;
@@ -17,9 +41,6 @@ contract RealEstateProperty {
         uint256[] rating;
         bool onSale;
     }
-
-    mapping(uint256 => Property) private properties;
-    uint public propertyIndex;
 
     // EVENTS
     event PropertyCreated(
@@ -36,13 +57,13 @@ contract RealEstateProperty {
     event PropertyListed(
         uint256 indexed productId,
         string productTitle,
-        address indexed owner,
+        address indexed owner
     );
 
     event PropertyUnlisted(
         uint256 indexed productId,
         string productTitle,
-        address indexed owner,
+        address indexed owner
     );
 
     event PropertySold(
@@ -60,7 +81,7 @@ contract RealEstateProperty {
     event PropertyResold(
         uint256 indexed productId,
         string productTitle,
-        address indexed oldOwner, 
+        address indexed oldOwner,
         address indexed newOwner,
         uint256 price,
         string category,
@@ -69,57 +90,54 @@ contract RealEstateProperty {
         string imageUrl
     );
 
-
     // Review
     struct Review {
         address reviewer;
-        string review;
-        uint256 rating;
-        string timestamp;
         string comment;
+        uint256 rating;
+        uint256 timestamp;
         uint256 likes;
-        uint256 dislikes; 
+        uint256 dislikes;
     }
-     
-     struct Product {
+
+    struct Product {
         uint256 productId;
         uint256 totalRating;
         uint256 totalReviews;
         uint256 totalLikes;
         uint256 totalDislikes;
-     }
+    }
 
     //MAPPINGS
-    mapping(uint256 => Review) private reviews;
+    mapping(uint256 => Review[]) private reviews;
     mapping(uint256 => Product) private products;
-    mapping(address => uint256) private userReviews;
+    mapping(address => uint256[]) private userReviews;
 
-    uint256 public reviewsCount;
+    
 
     event ReviewCreated(
         uint256 indexed productId,
         address indexed reviewer,
         string review,
         uint256 rating,
-        string timestamp,
-        string comment
+        uint256 timestamp
     );
 
     event ReviewLiked(
         uint256 indexed productId,
         uint256 indexed revieverIndex,
         address indexed liker,
-        uint256 likes
     );
 
     event ReviewDisliked(
         uint256 indexed productId,
         uint256 indexed revieverIndex,
         address indexed disliker,
-        uint256 dislikes
     );
 
     // FUNCTIONS
+
+    event Withdrawal(address indexed user, uint256 amount);
 
     function creacteProperty(
         string memory _productTitle,
@@ -129,7 +147,7 @@ contract RealEstateProperty {
         string memory _location,
         string memory _description,
         string memory _imageUrl
-    ) external returns(uint256) {
+    ) external returns (uint256) {
         require(price > 0, "Price must be greater than 0");
         uint256 productId = propertyIndex++;
 
@@ -143,9 +161,17 @@ contract RealEstateProperty {
         property.description = _description;
         property.imageUrl = _imageUrl;
 
-        emit PropertyCreated(productId, _productTitle, owner, price, _category, _location, _description, _imageUrl);
+        emit PropertyCreated(
+            productId,
+            _productTitle,
+            owner,
+            price,
+            _category,
+            _location,
+            _description,
+            _imageUrl
+        );
         return productId;
-
     }
 
     function listProperty(uint256 productId) external {
@@ -160,18 +186,34 @@ contract RealEstateProperty {
         emit PropertyUnlisted(productId, property.productTitle, property.owner);
     }
 
-    function updatePropertyPrice(uint256 productId, uint256 newPrice) external {
+    function updatePropertyPrice(uint256 productId, uint256 newPrice)
+        external
+        returns (string memory)
+    {
         Property storage property = properties[productId];
         require(newPrice > 0, "Price must be greater than 0");
         require(property.onSale == true, "Property is not on sale");
-        require(conditions: property.owner == msg.sender, "You are not the owner of this property");
+        require(
+            property.owner == msg.sender,
+            "You are not the owner of this property"
+        );
         properties[productId].price = newPrice;
 
+        return "Property price updated";
     }
 
-    function updateProprtyDetailes (uint256 productId, string memory _category, string memory _location, string memory _description, string memory _imageUrl) external returns(uint256) {
+    function updateProprtyDetailes(
+        uint256 productId,
+        string memory _category,
+        string memory _location,
+        string memory _description,
+        string memory _imageUrl
+    ) external returns (uint256) {
         Property storage property = properties[productId];
-        require(conditions: property.owner == msg.sender, "You are not the owner of this property");
+        require(
+            property.owner == msg.sender,
+            "You are not the owner of this property"
+        );
         property.category = _category;
         property.location = _location;
         property.description = _description;
@@ -179,10 +221,233 @@ contract RealEstateProperty {
 
         return productId;
     }
+
+    function buyProperty(uint256 productId) external payable {
+        Property storage property = properties[productId];
+        require(
+            property.owner != msg.sender,
+            "You cannot buy your own property"
+        );
+        require(property.onSale == true, "Property is not on sale");
+        require(msg.value >= property.price, "Insufficient balance");
+
+        uint256 commissionAmount = (property.price * commissionRate) / 100;
+        uint256 sellerAmount = property.price - commissionAmount;
+
+        pendingWithdrawals[msg.sender] += sellerAmount;
+        pendingWithdrawals[address(this)] += commissionAmount;
+
+        property.onSale = false;
+        property.owner = msg.sender;
+        emit PropertySold(
+            productId,
+            property.productTitle,
+            property.owner,
+            msg.sender,
+            property.price,
+            property.category,
+            property.location,
+            property.description,
+            property.imageUrl
+        );
+    }
+
+    function withdraw() external {
+        uint256 amount = pendingWithdrawals[msg.sender];
+        require(amount > 0, "Nothing to withdraw");
+
+        pendingWithdrawals[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+
+        emit Withdrawal(msg.sender, amount);
+    }
+
+    function withdrawSellerFunds() external {
+        uint256 amount = pendingWithdrawals[msg.sender];
+        require(amount > 0, "Nothing to withdraw");
+
+        pendingWithdrawals[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+
+        emit Withdrawal(msg.sender, amount);
+    }
+
+    function withdrawContractOwnerFunds() external onlyOwner {
+        uint256 amount = pendingWithdrawals[address(this)];
+        require(amount > 0, "Nothing to withdraw");
+
+        pendingWithdrawals[address(this)] = 0;
+        payable(contractOwner).transfer(amount);
+
+        emit Withdrawal(contractOwner, amount);
+    }
+
+    function getPropertiesOnSale() external view returns (Property[] memory) {
+        uint256 length = propertyIndex;
+        uint256 currentIdex = 0;
+        Property[] memory propertyArray = new Property[](length);
+        for (uint256 i = 0; i < length; i++) {
+            Property storage property = properties[i];
+            if (property.onSale == true) {
+                propertyArray[currentIdex] = property;
+                currentIdex++;
+            }
+        }
+        return propertyArray;
+    }
+
+    function getAllProperties() external view returns (Property[] memory) {
+        uint256 length = propertyIndex;
+        Property[] memory propertyArray = new Property[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            uint256 currentId = i + 1;
+            Property storage property = properties[currentId];
+            propertyArray[i] = property;
+        }
+
+        return propertyArray;
+    }
+
+    function getProperty(uint256 productId)
+        external
+        view
+        returns (
+            uint256,
+            address,
+            string memory,
+            uint256,
+            bool,
+            string memory,
+            string memory,
+            string memory
+        )
+    {
+        Property storage property = properties[productId];
+        return (
+            property.productId,
+            property.owner,
+            property.productTitle,
+            property.price,
+            property.onSale,
+            property.category,
+            property.description,
+            property.imageUrl
+        );
+    }
+
+    function getUserProperties(address user)
+        external
+        view
+        returns (Property[] memory)
+    {
+        uint256 propertyCount = 0;
+
+        // Loop through all possible product IDs
+        for (uint256 i = 1; i <= propertyIndex; i++) {
+            if (properties[i].owner == user) {
+                propertyCount++;
+            }
+        }
+
+        Property[] memory propertyArray = new Property[](propertyCount);
+        uint256 currentIdex = 0;
+
+        // Loop again to fetch properties owned by the user
+        for (uint256 i = 1; i <= propertyIndex; i++) {
+            if (properties[i].owner == user) {
+                Property storage property = properties[i];
+                propertyArray[currentIdex] = property;
+                currentIdex++;
+            }
+        }
+
+        return propertyArray;
+    }
+
+    //REVIEW FUNCTIONS
+
+    function createReview(
+        uint256 productId,
+        uint256 rating,
+        string calldata comment
+    ) external {
+        require(rating >= 1 && rating <= 5, "Rating must be between 1 and 5");
+        uint256 timestamp = block.timestamp;
+        address user = msg.sender;
+
+        Property storage property = properties[productId];
+
+        property.reviewers.push(user);
+        property.review.push(comment);
+        property.rating.push(rating);
+
+        // Add review to reviews mapping
+        reviews[productId].push(Review(user, comment, rating, timestamp, 0, 0));
+
+        // Push productId to userReviews mapping
+        userReviews[user].push(productId);
+
+        // Update product review details
+        products[productId].totalReviews += 1; // Increment total reviews count
+        products[productId].totalRating += rating; // Add to total rating
+
+        emit ReviewCreated(productId, user, comment, rating, timestamp);
+
+        reviewsCount++;
+    }
+
+    function getProductReviews(uint256 productId)
+        external
+        view
+        returns (Review[] memory)
+    {
+        return reviews[productId];
+    }
+
+    function getUserReviews(address user)
+        external
+        view
+        returns (Review[] memory)
+    {
+        // Get the length of the user's reviews list
+        uint256 length = userReviews[user].length;
+
+        // Initialize an array to store the user's reviews
+        Review[] memory reviewArray = new Review[](length);
+
+        // Loop through each review associated with the user
+        for (uint256 i = 0; i < length; i++) {
+            // Fetch the current product ID associated with the review
+            uint256 productId = userReviews[user][i];
+
+            // Fetch all reviews associated with this product ID
+            Review[] memory userReview = reviews[productId];
+
+            // Loop through each review in the product's review list
+            for (uint256 j = 0; j < userReview.length; j++) {
+                // Check if the review was authored by the user
+                if (userReview[j].reviewer == user) {
+                    // Assign the user's review to the review array
+                    reviewArray[i] = userReview[j];
+                }
+            }
+        }
+        return reviewArray;
+    }
+
+    function likeReview(uint256 productId, uint256 reviewIndex) external {
+        Review storage review = reviews[productId][reviewIndex];
+        review.likes++;
+        emit ReviewLiked(productId, reviewIndex, msg.sender);
+    }
+
+    function dislikeReview(uint256 productId, uint256 reviewIndex) external {
+        Review storage review = reviews[productId][reviewIndex];
+        review.dislikes++;
+        emit ReviewDisliked(productId, reviewIndex, msg.sender);
+    }
+
     
-
-
-
-
 
 }
