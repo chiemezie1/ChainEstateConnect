da730696408200fc0d1deeb3b95554313511076b
chiemezie1 <chiemezieagbo1@gmail.com>
2023-12-27 20:05:58 +0100

    react update

--- chainestateconnect/hardhat.config.js
+++ chainestateconnect/hardhat.config.js
@@ -1,43 +1,42 @@
-require("@matterlabs/hardhat-zksync-solc");
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_URL, PRIVATE_KEY } = process.env;
+console.log({ API_URL, PRIVATE_KEY });
+
+
+// module.exports = {
+//   defaultNetwork: "polygon_mumbai",
+//   networks: {
+//     hardhat: {
+//       chainId: 80001,
+//     },
+//     sepolia: {
+//       url:API_URL || "https://rpc.ankr.com/polygon_mumbai",
+//       accounts:  [`0x${PRIVATE_KEY}`],
+//     }
+//   },
+//   solidity: {
+//     version: "0.8.9",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200
+//       }
+//     }
+//   },
+// }
+
+require("@nomicfoundation/hardhat-toolbox");
+
+
+
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
+  solidity: "0.8.9",
   networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+    sepolia: {
+      url: `https://eth-goerli.g.alchemy.com/v2/OK5rgXixhGWUSeH4Vi6ZiP6B2OUfDLqx`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
