d656682dd5f87dad23af2dd7909648799e6ec15b
chiemezie1 <chiemezieagbo1@gmail.com>
2023-12-28 01:49:00 +0100

    Web3Modal added

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
 
+import { Web3Modal } from "../context/Web3Modal";
+
+export const metadata = {
+  title: "Web3Modal",
+  description: "Web3Modal Example",
+};
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3Modal>
+      <Component {...pageProps} />
+    </Web3Modal>
+  );
 }
