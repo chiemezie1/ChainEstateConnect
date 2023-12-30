5192b8776f43c60208e9b60efe9c171abd764421
chiemezie1 <chiemezieagbo1@gmail.com>
2023-12-30 09:26:17 +0100

    update

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,13 +1,13 @@
 
 import Image from 'next/image'
-import connectButton from './_components/connectButton';
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
     <h1>Hello World</h1>
-    <connectButton />
+
     <w3m-button />
     </main>
   )
