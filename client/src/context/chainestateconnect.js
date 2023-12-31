03314ba63e833cc50b0eb0a78519df783c7e5746
chiemezie1 <chiemezieagbo1@gmail.com>
2023-12-31 22:20:29 +0100

    update

--- client/src/context/chainestateconnect.js
+++ client/src/context/chainestateconnect.js
@@ -24,12 +24,58 @@ async function buyProperty(productId) {
 }
 
 async function createProperty(_productTitle, price, _category, _location, _description, _imageUrl) {
-    const provider = new ethers.providers.JsonRpcProvider('YOUR_RPC_URL'); // Replace with your RPC URL
+    const provider = new ethers.providers.JsonRpcProvider('YOUR_RPC_URL');
     const signer = provider.getSigner();
     const contract = createContractInstance(signer);
 
     try {
         const transaction = await contract.creacteProperty(_productTitle, price, _category, _location, _description, _imageUrl);
+        await transaction.wait();
+        console.info('Contract call success');
+    } catch (err) {
+        console.error('Contract call failure', err);
+    }
+}
+
+async function createReview(productId, rating, comment) {
+    const provider = new ethers.providers.JsonRpcProvider('YOUR_RPC_URL'); // Replace with your RPC URL
+    const signer = provider.getSigner();
+
+    const contract = createContractInstance(signer);
+
+    try {
+        const transaction = await contract.createReview(productId, rating, comment);
+        await transaction.wait();
+        console.info('Contract call success');
+    } catch (err) {
+        console.error('Contract call failure', err);
+    }
+}
+
+
+async function likeReview(productId, reviewIndex) {
+    const provider = new ethers.providers.JsonRpcProvider('YOUR_RPC_URL');
+    const signer = provider.getSigner();
+
+    const contract = createContractInstance(signer);
+
+    try {
+        const transaction = await contract.likeReview(productId, reviewIndex);
+        await transaction.wait();
+        console.info('Contract call success');
+    } catch (err) {
+        console.error('Contract call failure', err);
+    }
+}
+
+async function dislikeReview(productId, reviewIndex) {
+    const provider = new ethers.providers.JsonRpcProvider('YOUR_RPC_URL');
+    const signer = provider.getSigner();
+
+    const contract = createContractInstance(signer);
+
+    try {
+        const transaction = await contract.dislikeReview(productId, reviewIndex);
         await transaction.wait();
         console.info('Contract call success');
     } catch (err) {
