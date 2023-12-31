7c622c1664bc90d20d03696e9a055537b700e425
chiemezie1 <chiemezieagbo1@gmail.com>
2023-12-31 22:06:55 +0100

    update

--- client/src/context/chainestateconnect.js
+++ client/src/context/chainestateconnect.js
@@ -1,5 +1,4 @@
 import { ethers } from "ethers";
-
 import RealEstateProperty from "../Contract/RealEstateProperty.json";
 
 const fetchContract = (SignerOrProvider) => {
@@ -10,19 +9,31 @@ const fetchContract = (SignerOrProvider) => {
     );
 };
 
-
-
 async function buyProperty(productId) {
     const provider = new ethers.providers.JsonRpcProvider('YOUR_RPC_URL'); // Replace with your RPC URL
     const signer = provider.getSigner();
     const contract = fetchContract(signer);
 
     try {
-      const transaction = await contract.buyProperty(productId);
-      await transaction.wait();
-      console.info('Contract call success');
+        const transaction = await contract.buyProperty(productId);
+        await transaction.wait();
+        console.info('Contract call success');
     } catch (err) {
-      console.error('Contract call failure', err);
+        console.error('Contract call failure', err);
     }
-  }
-  
+}
+
+async function createProperty(_productTitle, price, _category, _location, _description, _imageUrl) {
+    const provider = new ethers.providers.JsonRpcProvider('YOUR_RPC_URL'); // Replace with your RPC URL
+    const signer = provider.getSigner();
+    const contract = createContractInstance(signer);
+
+    try {
+        const transaction = await contract.creacteProperty(_productTitle, price, _category, _location, _description, _imageUrl);
+        await transaction.wait();
+        console.info('Contract call success');
+    } catch (err) {
+        console.error('Contract call failure', err);
+    }
+}
+
